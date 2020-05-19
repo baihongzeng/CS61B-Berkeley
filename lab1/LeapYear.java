c5c051b387705fd1e01007909ad312bff649e98e
baihongzeng <baihongzeng@hotmail.com>
2020-05-20 05:30:42 +0800

    isLeapYear method completed

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author baihong zeng
  */
 public class LeapYear {
 
@@ -12,6 +12,17 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    /** check if a year is a leap year or not
+    *   @param year to be evaluated
+    */
+    public static boolean isLeapYear (int year) {
+    	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
