9122f3e44e37dca9f4271389296f71899a2dca4f
baihongzeng <baihongzeng@hotmail.com>
2020-05-21 20:02:47 +0800

    completed project 0

---
+++ proj0/NBody.java
@@ -0,0 +1,99 @@
+public class NBody {
+	/*get the radius of the universe, i.e. the drawing range for the map
+	  the structure of 'file_name' can be found in /data/3body.txt
+	*/
+
+	public static void main (String[] args) {
+		/*loading parameters*/
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+
+		String filename = args[2];
+		double radius = readRadius(filename);
+		Planet[] allPlanets = readPlanets(filename);
+
+		/*drawing the background*/
+ 		drawBackground(radius);
+ 		/* Draw Planets */
+	    drawPlanets(allPlanets);
+
+	    /* Show */
+	    StdDraw.show();
+
+	    /* Animations */
+	    StdDraw.enableDoubleBuffering();
+	    double t = 0;
+	    while (t < T) {
+			/* calc forces */
+			double[] xForces = new double[allPlanets.length];
+			double[] yForces = new double[allPlanets.length];
+
+			for (int i = 0; i < allPlanets.length; ++i) {
+				Planet p1 = allPlanets[i];
+				xForces[i] = p1.calcNetForceExertedByX(allPlanets);
+				yForces[i] = p1.calcNetForceExertedByY(allPlanets);
+			}
+		
+
+			/* update */
+			for (int i = 0; i < allPlanets.length; ++i) {
+				Planet p = allPlanets[i];
+				p.update(dt, xForces[i], yForces[i]);
+				drawBackground(radius);
+				drawPlanets(allPlanets);
+				StdDraw.show();
+				StdDraw.pause(1);
+			}
+
+			t += dt;
+		}
+	}
+
+	public static double readRadius (String file_name) {
+		In in = new In(file_name);
+
+		/* Every time you call a read method from the In class,
+		 * it reads the next thing from the file, assuming it is
+		 * of the specified type. */
+		int firstItemInFile = in.readInt();
+		double secondItemInFile = in.readDouble();
+
+		return secondItemInFile;
+	}
+
+	/* Given a file name, it should return an array of Planets 
+	corresponding to the allPlanets in the file*/
+	public static Planet[] readPlanets (String file_name) {
+		In in = new In(file_name);
+
+		int planet_num = in.readInt();
+		Planet[] result = new Planet[planet_num];
+
+		double universe_radius = in.readDouble();
+
+		for (int i = 0; i < planet_num; i++) {
+			 double xxPos = in.readDouble();
+			 double yyPos = in.readDouble();
+			 double xxVel = in.readDouble();
+			 double yyVel = in.readDouble();
+			 double mass = in.readDouble();
+			 String imgFileName = in.readString();
+			 result[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName); 
+		}		
+
+		return result;
+	}
+
+	private static void drawBackground(double radius) {
+	    StdDraw.setScale(-radius, radius);
+	    StdDraw.clear();
+	    StdDraw.picture(0, 0, "images/starfield.jpg");
+  	}
+
+  	private static void drawPlanets(Planet[] allPlanets) {
+	    for (Planet p : allPlanets) {
+	    	StdDraw.picture(p.xxPos, p.yyPos, "images/" + p.imgFileName);
+	      // p.draw();
+	    }
+  	}
+}
---
+++ proj0/Planet.java
@@ -0,0 +1,89 @@
+public class Planet {
+	public static final double G = 6.67e-11; //gravitational constant
+	public double xxPos; //current x position
+	public double yyPos; //current y position
+	public double xxVel; //current x velocity
+	public double yyVel; //current y velocity
+	public double mass; // mass
+	public String imgFileName;
+
+	// constructor
+	public Planet (double xP, double yP, double xV, 
+		double yV, double m, String img) {
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+	// overloaded constructor
+	public Planet (Planet p) {
+		this.xxPos = p.xxPos;
+		this.yyPos = p.yyPos;
+		this.xxVel = p.xxVel;
+		this.yyVel = p.yyVel;
+		this.mass = p.mass;
+		this.imgFileName = p.imgFileName;
+	}
+
+	/** calculates the distance between two Planets, one is itself, the other one is other planet*/
+	public double calcDistance (Planet otherPlanet) {
+		return Math.sqrt(Math.pow(this.xxPos - otherPlanet.xxPos, 2) + Math.pow(this.yyPos - otherPlanet.yyPos, 2));
+	}
+
+	/* returns the force exerted on this planet by the given planet*/
+	public double calcForceExertedBy (Planet p) {
+		double force = G * this.mass * p.mass / Math.pow(calcDistance(p), 2);
+		return force;
+	}
+
+	/*describe the force exerted by a given planet in the X directions*/
+	public double calcForceExertedByX (Planet p) {
+		double force_x = 0;
+		if (calcDistance(p) != 0) {
+			force_x = calcForceExertedBy(p) * (p.xxPos - this.xxPos) / calcDistance(p);	
+		}
+		return force_x;
+	}	
+	
+	/*describe the force exerted by a given planet in the Y directions*/
+	public double calcForceExertedByY (Planet p) {
+		double force_y = 0;
+		if (calcDistance(p) != 0) {
+			force_y = calcForceExertedBy(p) * (p.yyPos - this.yyPos) / calcDistance(p);	
+		}
+		return force_y;
+	}	
+
+	/*describe the force exerted by all other planets in the X directions*/
+	public double calcNetForceExertedByX (Planet[] allPlanets) {
+		double force_net_x = 0;
+		for (Planet p : allPlanets) {
+			force_net_x += calcForceExertedByX(p);
+		}
+
+		return force_net_x;
+	}
+
+	/*describe the force exerted by all other planets in the Y directions*/
+	public double calcNetForceExertedByY (Planet[] allPlanets) {
+		double force_net_y = 0;
+		for (Planet p : allPlanets) {
+			force_net_y += calcForceExertedByY(p);
+		}
+
+		return force_net_y;
+	}
+
+	public void update (double dt, double force_net_x, double force_net_y) {
+		double acceleration_x = force_net_x / this.mass;
+		double acceleration_y = force_net_y / this.mass;
+		
+		this.xxVel = this.xxVel + acceleration_x * dt;
+		this.yyVel = this.yyVel + acceleration_y * dt;
+
+		this.xxPos = this.xxPos + this.xxVel * dt;
+		this.yyPos = this.yyPos + this.yyVel * dt;
+	}
+}
