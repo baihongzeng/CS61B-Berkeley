674945e5b18974e9a0d5171070dde7a4b132ad50
baihongzeng <baihongzeng@hotmail.com>
2020-05-24 23:01:18 +0800

    fixed bugs in ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,7 +1,7 @@
 public class ArrayDeque<T> {
     private T[] item;
-    private int start;
-    private int end;
+    private int start = 0;
+    private int end = 0;
     private int sizeArray = 8;
     private final double expandRatio = 1.5;
 
@@ -15,13 +15,13 @@ public class ArrayDeque<T> {
 
     /*Adds an item of type T to the front of the deque.*/
     public void addFirst(T x) {
-        if (end - start + 1 >= sizeArray) {
+        if (end - start >= sizeArray) {
             resize();
         }
 
         if (start == 0) {
             T temp = x;
-            for (int i = 0; i <= end - start + 1; i++) {
+            for (int i = 0; i <= end - start; i++) {
                 T temp2 = item[i];
                 item[i] = temp;
                 temp = temp2;
@@ -34,7 +34,7 @@ public class ArrayDeque<T> {
 
     /*Adds an item of type T to the back of the deque.*/
     public void addLast(T x) {
-        if (end - start + 1 >= sizeArray) {
+        if (end - start >= sizeArray) {
             resize();
         }
         item[end] = x;
@@ -43,19 +43,19 @@ public class ArrayDeque<T> {
 
     /*Returns true if deque is empty, false otherwise.*/
     public boolean isEmpty() {
-        return (end - start + 1) == 0;
+        return (end - start) == 0;
     }
 
     /*Returns the number of items in the deque.*/
     public int size() {
-        return (end - start + 1);
+        return (end - start);
     }
 
     /*Prints the items in the deque from first to last, separated by a space.*/
     public void printDeque() {
-        for (int i = 0; i < (end - start + 1); i++) {
+        for (int i = 0; i < (end - start); i++) {
             //# of separator is one less than # of items
-            String separator = i < (end - start) ? " " : "";
+            String separator = i < (end - start - 1) ? " " : "";
             // the last item has no separator following it
             System.out.print(item[i] + separator);
         }
@@ -65,7 +65,7 @@ public class ArrayDeque<T> {
     /*Removes and returns the item at the front of the deque.
     If no such item exists, returns null.*/
     public T removeFirst() {
-        if (end - start + 1 <= 0) {
+        if (end - start <= 0) {
             return null;
         }
         T front = item[start];
@@ -78,7 +78,7 @@ public class ArrayDeque<T> {
     /*Removes and returns the item at the back of the deque.
     If no such item exists, returns null.*/
     public T removeLast() {
-        if (end - start + 1 <= 0) {
+        if (end - start <= 0) {
             return null;
         }
         T back = item[end - 1];
@@ -92,7 +92,7 @@ public class ArrayDeque<T> {
     where 0 is the front, 1 is the next item, and so forth.
     If no such item exists, returns null. Must not alter the deque!*/
     public T get(int index) {
-        if (end - start + 1 <= 0 || index < 0 || index >= sizeArray) {
+        if (end - start <= 0 || index < 0 || index >= sizeArray) {
             return null;
         }
         return item[start + index];
