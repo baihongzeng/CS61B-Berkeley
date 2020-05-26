47b83d12496ef6985f2a9be5750f9b159571b004
baihongzeng <baihongzeng@hotmail.com>
2020-05-26 22:47:16 +0800

    added template files to be done in prject 1b

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,201 @@
+/*use circular array to do! no linear array
+* useful circular array link: https://blog.csdn.net/yyoc97/article/details/88759562*/
+public class ArrayDeque<T> {
+    private T[] item;
+    private int start = 0;
+    private int end = 0;
+    private int sizeQueue = 0;
+    private int sizeArray = 8; // initial size of array
+    private final double expandRatio = 1.5;
+    private final double usageRatio = 0.25;
+
+//    public static void main(String[] args) {
+//        ArrayDeque array = new ArrayDeque();
+//        array.addFirst(0);
+//        array.get(0); //      ==> 0
+//        array.removeLast(); //      ==> 0
+//        array.addLast(3);
+//        array.addLast(4);
+//        array.addFirst(5);
+//        array.removeFirst(); //     ==> 5
+//        array.removeFirst(); //     ==> 4
+//    }
+
+    /*check if the array is separated into two segments or one continuous segment*/
+    private boolean endInFrontOfStart() {
+        return end <= start;
+    }
+
+    /*increase the size of embedded array*/
+    private void resize() {
+        int newLength = (int) (sizeArray * expandRatio);
+        T[] newArray = (T[]) new Object[newLength];
+        if (endInFrontOfStart()) { // end is in front of start, two segments
+            System.arraycopy(item, 0, newArray, 0, end); //copy mid - end portion
+            System.arraycopy(item, start,
+                    newArray, newLength - (sizeArray - start),
+                    sizeArray - start); // copy start - mid portion
+            start = newLength - (sizeArray - start);
+            end = end;
+        } else { // start is in front of end, one segment
+            System.arraycopy(item, start, newArray, 0, sizeQueue);
+            start = 0;
+            end = start + sizeQueue;
+        }
+        item = newArray;
+        sizeArray = newLength;
+    }
+
+    /*resize T[] item down when too many items are removed.*/
+    private void resizeDown() {
+        int newLength = (int) (sizeArray * usageRatio * 2);
+        T[] newArray = (T[]) new Object[newLength];
+
+        if (endInFrontOfStart()) { // end is in front of start, two segments
+            System.arraycopy(item, 0, newArray, 0, end); //copy mid - end portion
+            System.arraycopy(item, start, newArray, newLength - (sizeArray - start),
+                    sizeArray - start); // copy start - mid portion
+            start = newLength - (sizeArray - start);
+        } else {
+            System.arraycopy(item, start, newArray, 0, sizeQueue);
+            start = 0;
+            end = start + sizeQueue;
+        }
+        item = newArray;
+        sizeArray = newLength;
+    }
+
+    /*Adds an item of type T to the front of the deque.*/
+    public void addFirst(T x) {
+        if (sizeQueue >= sizeArray) {
+            resize();
+        }
+
+        if (start - 1 < 0) { // go to the back of array
+            start = sizeArray - 1;
+        } else {             // simply add in front
+            start = start - 1;
+        }
+        item[start] = x;
+        sizeQueue++;
+    }
+
+    /*Adds an item of type T to the back of the deque.*/
+    public void addLast(T x) {
+        if (sizeQueue >= sizeArray) {
+            resize();
+        }
+        item[end] = x;
+        if (end + 1 == sizeArray) { // go to start of the array
+            end = 0;
+        } else {                    // simply add in the back
+            end++;
+        }
+        sizeQueue++;
+    }
+
+    /*Returns true if deque is empty, false otherwise.*/
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /*Returns the number of items in the deque.*/
+    public int size() {
+        return sizeQueue;
+    }
+
+    /*Removes and returns the item at the front of the deque.
+    If no such item exists, returns null.*/
+    public T removeFirst() {
+        if (sizeQueue <= 0) {
+            return null;
+        } else if (sizeQueue < sizeArray * usageRatio) {
+            resizeDown();
+        }
+
+        T front = item[start];
+        item[start] = null; //reset the unused element to be null,
+        // so that no pointer is pointing to that memory, garbage collector can free it
+        if (start == sizeArray - 1) {
+            start = 0;
+        } else {
+            start++;
+        }
+        sizeQueue--;
+        return front;
+    }
+
+    /*Removes and returns the item at the back of the deque.
+    If no such item exists, returns null.*/
+    public T removeLast() {
+        if (sizeQueue <= 0) {
+            return null;
+        } else if (sizeQueue < sizeArray * usageRatio) {
+            resizeDown();
+        }
+
+        if (end == 0) {
+            end = sizeArray - 1;
+        } else {
+            end--;
+        }
+        T back = item[end];
+        item[end] = null; //reset the unused element to be null,
+        // so that no pointer is pointing to that memory, garbage collector can free it
+        sizeQueue--;
+        return back;
+    }
+
+    /*Gets the item at the given index,
+    where 0 is the front, 1 is the next item, and so forth.
+    If no such item exists, returns null. Must not alter the deque!*/
+    public T get(int index) {
+        if (sizeQueue <= 0 || index < 0 || index >= sizeArray) {
+            return null;
+        }
+
+        if (endInFrontOfStart()) {
+            if (start + index >= sizeArray) {
+                return item[start + index - sizeArray];
+            } else {
+                return item[start + index];
+            }
+        } else {
+            return item[start + index];
+        }
+
+    }
+
+    /*constructor*/
+    public ArrayDeque() {
+        item = (T[]) new Object[sizeArray];
+    }
+
+    /*Prints the items in the deque from first to last, separated by a space.*/
+    public void printDeque() {
+        if (endInFrontOfStart()) {
+            for (int i = start; i < sizeArray; i++) {
+                //# of separator is one less than # of items
+                String separator = (i - start < sizeQueue) ? " " : "";
+                // the last item has no separator following it
+                System.out.print(item[i] + separator);
+            }
+
+            for (int i = 0; i < end; i++) {
+                //# of separator is one less than # of items
+                String separator = i < (end - 1) ? " " : "";
+                // the last item has no separator following it
+                System.out.print(item[i] + separator);
+            }
+        } else {
+            for (int i = 0; i < (end - start); i++) {
+                //# of separator is one less than # of items
+                String separator = i < (end - start - 1) ? " " : "";
+                // the last item has no separator following it
+                System.out.print(item[i] + separator);
+            }
+        }
+
+        System.out.println();
+    }
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,139 @@
+public class LinkedListDeque<T> { // use circular linked list as data structure
+    /*Nested class, representing elements in linked list deque*/
+    private class Node { //should be private
+        private T item;
+        private Node prev;
+        private Node next;
+        // constructor, modifier the same as the class scope, i.e. private for private class
+        Node(T x) {
+            item = x;
+            prev = null;
+            next = null;
+        }
+    }
+
+    // instance variables, should be private
+    private Node head; // i.e. sentinel node
+    private int size;
+
+    /*Adds an item of type T to the front of the deque.
+     * time complexity: O(1)*/
+    public void addFirst(T item) {
+        Node current = new Node(item);
+        current.next = head.next;
+        head.next.prev = current;
+        current.prev = head;
+        head.next = current;
+
+        size += 1;
+    }
+
+    /* Adds an item of type T to the back of the deque.
+     * time complexity: O(1)*/
+    public void addLast(T item) {
+        Node current = new Node(item);
+        current.prev = head.prev;
+        head.prev.next = current;
+        current.next = head;
+        head.prev = current;
+
+        size += 1;
+    }
+
+    /*Returns true if deque is empty, false otherwise.*/
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /*Returns the number of items in the deque.*/
+    public int size() {
+        return size;
+    }
+
+    /*Removes and returns the item at the front of the deque.
+    If no such item exists, returns null.*/
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        Node first = head.next;
+        head.next = head.next.next; //update head.next
+        head.next.prev = head;
+
+        size -= 1;
+        return first.item;
+    }
+
+    /*Removes and returns the item at the back of the deque. If no such item exists, returns null.*/
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        Node last = head.prev;
+        head.prev = head.prev.prev; //update head.prev
+        head.prev.next = head;
+
+        size -= 1;
+        return last.item;
+    }
+
+    /*Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+    If no such item exists, returns null. Must not alter the deque!
+    time complexity: O(n)*/
+    public T get(int index) {
+        if (size == 0 || index >= size || index < 0) { //corner case
+            return null;
+        }
+        int count = 0;
+        Node current = head;
+        while (count < index) {
+            current = current.next;
+            count += 1;
+        }
+        return current.next.item;
+    }
+
+    /*Same as get, but uses recursion.*/
+    public T getRecursive(int index) {
+        if (size == 0 || index >= size || index < 0) { //corner case
+            return null;
+        }
+        Node current = head;
+        return getRecursiveHelper(index, current);
+    }
+
+    /* Helper function called by getRecursive() to do recursion*/
+    private T getRecursiveHelper(int index, Node current) {
+        if (index == 0) {
+            return current.next.item;
+        }
+        current = current.next;
+        index--;
+        return getRecursiveHelper(index, current);
+    }
+
+    /*Creates an empty linked list deque.*/
+    //用链表来做double ended queue, 用的是循环链表
+    public LinkedListDeque() {
+        head = new Node(null);
+        head.prev = head; // circular linked list
+        head.next = head;
+        size = 0;
+    }
+
+    /*Prints the items in the deque from first to last, separated by a space.*/
+    public void printDeque() {
+        Node current = head;
+        int count = 0;
+        while (count < size) {
+            //# of separator is one less than # of items
+            String separator = count < (size - 1) ? " " : "";
+            // the last item has no separator following it
+            System.out.print(current.next.item + separator);
+            current = current.next;
+            count++;
+        }
+        System.out.println();
+    }
+}
+
