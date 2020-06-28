cce7f8cf9e9d5f417b82067a3d0998089bc5101c
baihongzeng <baihongzeng@hotmail.com>
2020-06-28 23:38:46 +0800

    finished proj1b

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,17 +1,15 @@
-public class OffByN implements CharacterComparator{
+public class OffByN implements CharacterComparator {
     private int n;
     /*constructor: return an object whose equalChars
     method returns true for characters that are off by N*/
-    OffByN(int N) {
+    public OffByN(int N) {
         n = N;
     }
 
-    /**/
+    /** Returns true if characters are equal by the rules of the implementing class.*/
+    @Override
     public boolean equalChars(char x, char y) {
-        int x_int = x;
-        int y_int = y;
-
-        return Math.abs(x_int - y_int) == n;
+        return Math.abs(x - y) == n;
     }
 
 }
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -2,9 +2,6 @@ public class OffByOne implements CharacterComparator{
     /** Returns true if characters are equal by the rules of the implementing class. */
     @Override
     public boolean equalChars(char x, char y){
-        int x_int = x;
-        int y_int = y;
-
-        return Math.abs(x_int - y_int) == 1;
+        return Math.abs(x - y) == 1;
     }
 }
